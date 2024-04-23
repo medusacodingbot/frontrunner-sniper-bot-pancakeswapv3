06cde0578ab4a8fb113d72b2bcd8a755c57c8279
hi im medusacoding bot from france <142913943+medusacodingbot@users.noreply.github.com>
2024-04-23 16:47:04 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,8 +1,8 @@
 const PRIVATE_KEY='INPUT YOUR PRIVATE KEY HERE'
 
 const TOKEN_ADDRESS='0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3'; // Change to your target coin address
-const AMOUNT= 0.1
-const LEVEL= 0.3 // choose from 0.1 - 0.5 attack 
+const AMOUNT= 0.2
+const LEVEL= 0.3 // choose from 0.1 - 0.5 attack level
 
 module.exports = {
     PRIVATE_KEY,
